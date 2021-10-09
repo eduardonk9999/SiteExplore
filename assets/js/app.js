e228269582fb830e6e28fe58e288291a621c90d0
eduardonk9999 <thiago.novak7@gmail.com>
2021-10-09 16:33:54 -0300

    Add, animate

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,33 @@ function menuMobile() {
 }
 menuMobile()
 
+const windowHeight = window.innerHeight * 0.5;
+
+function animationScrollLeft() {
+  const intensScroll = document.querySelectorAll('.js-scroll')
+  intensScroll.forEach((item) => {
+    const sectionTopPostionWindow = item.getBoundingClientRect().top
+    const sectionActive = (sectionTopPostionWindow - windowHeight) < 0;
+
+   if(sectionActive) {
+     item.classList.add('active')
+   }
+  })
+}
+window.addEventListener('scroll', animationScrollLeft)
+
+function animationScrollRight() {
+  const intensScroll = document.querySelectorAll('.js-scroll-right')
+  intensScroll.forEach((item) => {
+    const sectionTopPostionWindow = item.getBoundingClientRect().top
+    const sectionActive = (sectionTopPostionWindow - windowHeight) < 0;
+
+   if(sectionActive) {
+     item.classList.add('active')
+   }
+  })
+
+
+}
+
+window.addEventListener('scroll', animationScrollRight)
